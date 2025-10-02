2ba7e8434003b497cf5a667c166e6fb70ea1dc98
Emiliano Bruni <info@ebruni.it>
2025-10-02 15:15:35 +0200

    simplify by removed crontab

--- examples/hello_world.ts
+++ examples/hello_world.ts
@@ -1,14 +1,6 @@
 import Kronos from '@/index.js';
-import path from 'path';
-import { unlinkSync } from 'node:fs';
-const cronTabPath = path.join(
-            process.cwd(),
-            'examples',
-            `hello_world-${Date.now()}.crontab`
-        );
 
 const cm = await Kronos.create({
-    cronTabPath,
     logger: { level: 'info' }
 });
 
@@ -19,7 +11,6 @@ cm.add({
         this.log && this.log.info('Hello, World!');
         this.stop();
         cm.close();
-        unlinkSync(cronTabPath);
     },
     start: true,
 });
