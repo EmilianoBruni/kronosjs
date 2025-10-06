2c3f567bada41bb6064bff45173ac69d65c03276
Emiliano Bruni <info@ebruni.it>
2025-10-06 13:17:56 +0200

    feat: enhance HttpServer with job management routes and Kronos integration

--- src/libs/HttpServer.ts
+++ src/libs/HttpServer.ts
@@ -1,17 +1,21 @@
 import Fastify, { FastifyInstance } from 'fastify';
 import { KLogOptions } from '@/types.js';
+import Kronos from './Kronos.js';
 
 class HttpServer {
     private fastify: FastifyInstance;
     private port: number;
     private host: string;
     private isStarted: boolean = false;
+    private kronos: Kronos;
 
     constructor(
+        kronos: Kronos,
         logConfig: boolean | KLogOptions | undefined,
         port: number = 3000,
         host: string = '0.0.0.0'
     ) {
+        this.kronos = kronos;
         this.port = port;
         this.host = host;
 
@@ -33,35 +37,84 @@ class HttpServer {
         this.fastify = Fastify({ logger });
 
         // Configure routes immediately after initialization
-        this.configureRoutes();
+        this.#configureRoutes();
     }
 
     /**
      * Configure server routes
      */
-    private configureRoutes(): void {
-        this.fastify.get('/health', async () => {
+    #configureRoutes(): void {
+        const f = this.fastify;
+        const { jobs } = this.kronos;
+        f.get('/health', async () => {
             return { status: 'ok' };
         });
-        // Add more routes as needed
-    }
+        // /api/jobs
+        f.get('/api/jobs', async () => {
+            const jobsArray = Array.from(jobs.values()).map(job => job.name);
+            return { total: jobsArray.length, items: jobsArray };
+        });
 
-    /**
-     * Add custom route to the server
-     */
-    // public addRoute(
-    //     method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH',
-    //     url: string,
-    //     handler: (request: any, reply: any) => any
-    // ): void {
-    //     const methodName = method.toLowerCase() as
-    //         | 'get'
-    //         | 'post'
-    //         | 'put'
-    //         | 'delete'
-    //         | 'patch';
-    //     this.fastify[methodName](url, handler);
-    // }
+        // /api/jobs/:jobName
+        f.get('/api/jobs/:jobName', async (request, reply) => {
+            const { jobName } = request.params as { jobName: string };
+            const job = jobs.get(jobName);
+            if (!job) {
+                reply.status(404);
+                return { error: 'Job not found' };
+            }
+            return {
+                name: job.name,
+                cronTime: job.cronTime,
+                isActive: job.isActive,
+                isRunning: job.isCallbackRunning,
+                lastDate: job.lastDate,
+                nextDate: job.nextDate
+            };
+        });
+
+        // /api/jobs/:jobName/start
+        f.post('/api/jobs/:jobName/start', async (request, reply) => {
+            const { jobName } = request.params as { jobName: string };
+            const job = jobs.get(jobName);
+            if (!job) {
+                reply.status(404);
+                return { result: false, status: 'Job not found' };
+            }
+            if (job.isActive) {
+                return { result: true, status: 'Job is already running' };
+            }
+            job.start();
+            return { result: true, status: 'Job started' };
+        });
+
+        // /api/jobs/:jobName/stop
+        f.post('/api/jobs/:jobName/stop', async (request, reply) => {
+            const { jobName } = request.params as { jobName: string };
+            const job = jobs.get(jobName);
+            if (!job) {
+                reply.status(404);
+                return { result: false, status: 'Job not found' };
+            }
+            if (!job.isActive) {
+                return { result: true, status: 'Job is not running' };
+            }
+            job.stop();
+            return { result: true, status: 'Job stopped' };
+        });
+
+        // DELETE api/jobs/:jobName
+        f.delete('/api/jobs/:jobName', async (request, reply) => {
+            const { jobName } = request.params as { jobName: string };
+            const job = jobs.get(jobName);
+            if (!job) {
+                reply.status(404);
+                return { result: false, status: 'Job not found' };
+            }
+            await this.kronos.remove(jobName, true);
+            return { result: true, status: 'Job removed' };
+        });
+    }
 
     /**
      * Start the HTTP server
